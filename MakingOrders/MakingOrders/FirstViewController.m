53affd51065c51f371b5869f1637cf29135ba77d
Robert P. Darchuk <NovaProgramming@GMail.com>
2014-02-13 15:33:50 -0800

    Something

--- MakingOrders/MakingOrders/FirstViewController.m
+++ MakingOrders/MakingOrders/FirstViewController.m
@@ -26,4 +26,36 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"catalogCell"];
+    if (cell == nil)
+    {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"catalogCell"];
+    }
+//    
+//    NSDictionary *event = self.ghEvents[indexPath.row];
+//    cell.detailTextLabel.text = [NSString stringWithFormat:@"%@ %@", event[@"type"], event[@"created_at"]];
+//    NSString *user = [event valueForKeyPath:@"actor.login"];
+//    NSString *desc = [event valueForKeyPath:@"payload.description"];
+//    if (desc == nil)
+//    {
+//        desc = [event valueForKeyPath:@"repo.name"];
+//    }
+//    
+//    NSString *avatarURLStr = [event valueForKeyPath:@"actor.avatar_url"];
+//    [cell.imageView setImageWithURL:[NSURL URLWithString:avatarURLStr] placeholderImage:[UIImage imageNamed:@"flowers.jpg"]];
+//    
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"BORP"];
+    
+    return cell;
+}
+
+
 @end
