84a7127a11fa82f03ec063a8a4c6030ea00b5ff5
Robert P. Darchuk <NovaProgramming@GMail.com>
2014-02-13 19:49:00 -0800

    Stuff and stuff

---
+++ MakingOrders/MakingOrders/ItemStatusViewController.h
@@ -0,0 +1,13 @@
+//
+//  ItemStatusViewController.h
+//  MakingOrders
+//
+//  Created by DKC_User on 2/13/14.
+//  Copyright (c) 2014 Jon Bott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ItemStatusViewController : UIViewController
+
+@end
---
+++ MakingOrders/MakingOrders/ItemStatusViewController.m
@@ -0,0 +1,38 @@
+//
+//  ItemStatusViewController.m
+//  MakingOrders
+//
+//  Created by DKC_User on 2/13/14.
+//  Copyright (c) 2014 Jon Bott. All rights reserved.
+//
+
+#import "ItemStatusViewController.h"
+
+@interface ItemStatusViewController ()
+
+@end
+
+@implementation ItemStatusViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
