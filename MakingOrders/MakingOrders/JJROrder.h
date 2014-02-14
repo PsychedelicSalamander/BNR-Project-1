64b6548a7130b6e6d2c2c40c9a919593e7bf4d6b
Jay DeBoer <jaydeboer@gmail.com>
2014-02-13 16:12:15 -0800

    base of JJROrder

---
+++ MakingOrders/MakingOrders/JJROrder.h
@@ -0,0 +1,13 @@
+//
+//  JJROrder.h
+//  MakingOrders
+//
+//  Created by Jay on 2/13/14.
+//  Copyright (c) 2014 Jon Bott. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JJROrder : NSObject
+
+@end
---
+++ MakingOrders/MakingOrders/JJROrder.m
@@ -0,0 +1,13 @@
+//
+//  JJROrder.m
+//  MakingOrders
+//
+//  Created by Jay on 2/13/14.
+//  Copyright (c) 2014 Jon Bott. All rights reserved.
+//
+
+#import "JJROrder.h"
+
+@implementation JJROrder
+
+@end
