c511c3b14cb1f4537a5b3d40b2288e01c24a4429
Jon Bott <jbott@1800contacts.com>
2014-02-13 16:28:32 -0800

    Added Notification

--- MakingOrders/MakingOrders/DataManager.m
+++ MakingOrders/MakingOrders/DataManager.m
@@ -16,24 +16,22 @@ NSMutableArray *_items;
 
 + (void)loadProducts
 {
-
 	_items = @[].mutableCopy;
 
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
     
     manager.responseSerializer = [AFJSONResponseSerializer serializer];//[JSONResponseSerializerWithData serializer];
-    //    [manager POST:@"https://api.github.com/events" parameters:@{@"key1": @"value1", @"key2": @"value2"}
     [manager GET:@"http://bnr-fruititems.appspot.com/" parameters:@{}
          success:^(AFHTTPRequestOperation *operation, id responseObject) {
              
              NSLog(@"success %@", responseObject);
-             [[self class] generateCatalog:responseObject];
-//             self.ghEvents = responseObject;
-//             [NSOperationQueue.mainQueue addOperationWithBlock:^{
-//                 [self.tableView reloadData];
-//             }];
-             
-         }failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+			 [[self class] parseProducts:responseObject];
+             [NSOperationQueue.mainQueue addOperationWithBlock:^{
+				 [[self class] loadHistory];
+             }];
+
+         }
+		 failure:^(AFHTTPRequestOperation *operation, NSError *error) {
              
              // get the json here
              //        id json = error.userInfo[JSONResponseSerializerWithDataKey];
@@ -43,14 +41,13 @@ NSMutableArray *_items;
 
 }
 
-+ (void)generateCatalog:(id)responseObject
++ (void)parseProducts:(id)responseObject
 {
 //				"color" : ["#000000", "#ff0000", "#00ff00", "#0000ff"],
 //				"sizes" : ["Small", "Medium", "Large", "X-Large", "2X-Large"]
 
 	for(NSDictionary *item in responseObject)
 	{
-
 		JJRCatalogItemCustomization *customization = [JJRCatalogItemCustomization new];
 		customization.text = [item valueForKeyPath:@"customizations.text"] ?: @"";
 		customization.logo = [item valueForKeyPath:@"customizations.logo"] ?: @"";
@@ -65,18 +62,22 @@ NSMutableArray *_items;
 
 		[_items addObject:catalogItem];
 	}
-	
-	NSLog(@"%@", _items);
-}
 
-+ (void)setItems:(id)items
-{
-	_items = items;
+	NSLog(@"%@", _items);
+    dispatch_async(dispatch_get_main_queue(), ^{
+		[NSNotificationCenter.defaultCenter postNotificationName:kJJRCatalogReady object:self userInfo:nil];
+    });
+
 }
 
 + (NSArray *)items
 {
 	return _items;
+}
+
++ (void)loadHistory
+{
+
 }
 
 @end
